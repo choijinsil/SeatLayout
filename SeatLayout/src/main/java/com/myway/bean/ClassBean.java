b76fdcdf7c3d4188ae3e3efd3e0791cc57775a8c
choijinsil <43313845+choijinsil@users.noreply.github.com>
2018-11-23 00:41:28 +0900

    init

---
+++ SeatLayout/src/main/java/com/myway/bean/ClassBean.java
@@ -0,0 +1,40 @@
+package com.myway.bean;
+
+/**
+ * 클래스에 대한 설명을 여기에 쓴다.
+ * 
+ * @author 	js
+ * @since 	2018. 11. 22.
+ * @version 1.0
+ * @see <pre>
+ *  == 개정이력(Modification Information) ==
+ *   
+ *   수정일			수정자				수정내용
+ *  ---------------------------------------------------------------------------------
+ *   2018. 11. 22.		js				최초생성
+ * 
+ * </pre>
+ */
+
+public class ClassBean{
+
+	private String firstName;
+	private String LastName;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return LastName;
+	}
+	public void setLastName(String lastName) {
+		LastName = lastName;
+	}
+	
+	
+	
+
+}
