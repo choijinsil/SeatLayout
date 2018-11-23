183ab45cd64669e917774d00e97e2b7fe415749f
sjc0502 <sjc0502@hunet.co.kr>
2018-11-23 10:29:07 +0900

    init

--- seatLayout/src/main/java/com/myway/seat/web/TestController.java
+++ seatLayout/src/main/java/com/myway/seat/web/TestController.java
@@ -2,29 +2,31 @@ package com.myway.seat.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * 클래스에 대한 설명을 여기에 쓴다.
  * 
- * @author 	sjc0502
- * @since 	2018. 11. 21.
+ * @author 	js
+ * @since 	2018. 11. 20.
  * @version 1.0
  * @see <pre>
  *  == 개정이력(Modification Information) ==
  *   
  *   수정일			수정자				수정내용
  *  ---------------------------------------------------------------------------------
- *   2018. 11. 21.		sjc0502				최초생성
+ *   2018. 11. 20.		js				최초생성
  * 
  * </pre>
  */
+// 리턴되는 화면이 있으면 Controller 아니면 RestController
 @Controller
 public class TestController {
-	
-	@RequestMapping(value="/test-page", method=RequestMethod.GET)
-	public String test() {
+	@RequestMapping("/testPage")
+	public String test(){
+		System.out.println("asd");
+		System.out.println("내가추가");
 		return "test";
 	}
+	
 
 }
--- seatLayout/src/main/java/com/myway/seat/web/TestRestController.java
+++ seatLayout/src/main/java/com/myway/seat/web/TestRestController.java
@@ -1,48 +1,91 @@
 package com.myway.seat.web;
 
+import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myway.bean.NameBean;
+
 /**
  * 클래스에 대한 설명을 여기에 쓴다.
  * 
- * @author 	sjc0502
- * @since 	2018. 11. 21.
+ * @author js
+ * @since 2018. 11. 20.
  * @version 1.0
- * @see <pre>
+ * @see
+ * 
+ *      <pre>
  *  == 개정이력(Modification Information) ==
  *   
  *   수정일			수정자				수정내용
  *  ---------------------------------------------------------------------------------
- *   2018. 11. 21.		sjc0502				최초생성
+ *   2018. 11. 20.		js				최초생성
  * 
- * </pre>
+ *      </pre>
  */
-
 @RestController
 public class TestRestController {
-	
-	@RequestMapping(value="/test", method=RequestMethod.GET)
+	// 화면에 값이 바로 나오게 할때 rest를 사용한다.
+	// 메이븐 종속성 추가시, maven update, clean, install해줘야 한다. 이때 에러가 안나면 사용가능한 것이다.
+	// 이 데이터를 화면에 뿌려줘.
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
 	public String test() {
-		return "test - restcontroller";
+		return "hello";
 	}
-	
-	@RequestMapping(value="/test", method=RequestMethod.POST)
-	public String test(String name) {
-		return name + ": 여기는 Post로만 호출 가능";
-	}
-	
-	@RequestMapping(value="/test-list", method=RequestMethod.GET)
-	public List<String> testList() {
+
+	@RequestMapping(value = "/test1", method = RequestMethod.GET)
+	public List<String> test1() {
 		List<String> list = new ArrayList<String>();
 		list.add("1");
 		list.add("22");
 		list.add("333");
 		list.add("4444");
+
 		return list;
 	}
+
+	@RequestMapping(value = "/test", method = RequestMethod.POST)
+	public String test(String name) {
+		return "by";
+	}	
+
+	// 같은것으론 GetMapping이 있음.
+	@RequestMapping(value = "/test_choice", method = RequestMethod.GET)
+	public List<NameBean> testChoice() {
+		
+		NameBean nameBean=new NameBean();
+		// generic 
+		// 클래스를 다시 만들땐 재시작 해줘야 함.
+		List<NameBean> list = new ArrayList<NameBean>();
+		
+		nameBean.setFirstName("my siri");
+		nameBean.setLastName("choi");
+		list.add(nameBean);
+		
+		nameBean=new NameBean();
+		
+		
+		nameBean.setFirstName("iloveu");
+		nameBean.setLastName("cho");
+		list.add(nameBean);
+		
+		
+		return list;
+	}
+
+//	@RequestMapping(value = "/test_list", method = RequestMethod.GET)
+//	public ClassBean testList(){
+//		//testChoice();
+//		ClassBean classBean=new ClassBean();
+//		classBean.setFirstName("iloveyou");
+//		classBean.setLastName("cho");
+//		return classBean;
+//	}
+	
 }
+	
